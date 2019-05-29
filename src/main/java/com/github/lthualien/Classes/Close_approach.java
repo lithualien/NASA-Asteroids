e8c637eb050810b118837328e8da6bb819efe890
TautvydasGustas1 <39690430+TautvydasGustas1@users.noreply.github.com>
2019-05-29 20:16:28 +0300

    Update Close_approach.java

--- src/main/java/com/github/lthualien/Classes/Close_approach.java
+++ src/main/java/com/github/lthualien/Classes/Close_approach.java
@@ -18,6 +18,15 @@ public class Close_approach {
     double miss_distance;
     String orbit_body;
 
+    /**
+     * 
+     * @param id
+     * @param asteroid_id
+     * @param close_approach_data
+     * @param relative_velocity
+     * @param miss_distance
+     * @param orbit_body 
+     */
     public Close_approach(int id, int asteroid_id, String close_approach_data, double relative_velocity, double miss_distance, String orbit_body) {
         this.id = id;
         this.asteroid_id = asteroid_id;
@@ -34,50 +43,99 @@ public class Close_approach {
         
     }
     
+    /**
+     * Geting ID
+     * @return 
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Setting ID
+     * @param id 
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Get asteroid id
+     * @return 
+     */
     public int getAsteroid_id() {
         return asteroid_id;
     }
 
+    /**
+     * Set asteroid id
+     * @param asteroid_id 
+     */
     public void setAsteroid_id(int asteroid_id) {
         this.asteroid_id = asteroid_id;
     }
+    
 
+    /**
+     * Get apprach appraoch data
+     * @return 
+     */
     public String getClose_approach_data() {
         return close_approach_data;
     }
 
+    /**
+     * Set close approach data
+     * @param close_approach_data 
+     */
     public void setClose_approach_data(String close_approach_data) {
         this.close_approach_data = close_approach_data;
     }
 
+    /**
+     * get relative velocity
+     * @return 
+     */
     public double getRelative_velocity() {
         return relative_velocity;
     }
 
+    /**
+     * set relative velocity
+     * @param relative_velocity 
+     */
     public void setRelative_velocity(double relative_velocity) {
         this.relative_velocity = relative_velocity;
     }
 
+    /**
+     * get miss distance
+     * @return 
+     */
     public double getMiss_distance() {
         return miss_distance;
     }
 
+    /**
+     * set miss distance
+     * @param miss_distance 
+     */
     public void setMiss_distance(double miss_distance) {
         this.miss_distance = miss_distance;
     }
 
+    /**
+     * get orbit data
+     * @return 
+     */
     public String getOrbit_body() {
         return orbit_body;
     }
 
+    /**
+     * set orbit data
+     * @param orbit_body 
+     */
     public void setOrbit_body(String orbit_body) {
         this.orbit_body = orbit_body;
     }
