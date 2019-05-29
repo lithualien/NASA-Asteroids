38108bcc5876bb8b23bbfb75bfa810c64248fd1b
Vygandas Eidukis <s033224@ad.viko.lt>
2019-05-29 22:46:17 +0300

    DAO class finished

--- src/main/java/com/github/lithualien/dao/Dao.java
+++ src/main/java/com/github/lithualien/dao/Dao.java
@@ -1,4 +1,14 @@
 package com.github.lithualien.dao;
 
+import com.github.lithualien.data.Asteroid;
+import com.sun.xml.internal.bind.v2.model.core.ID;
+
+import java.util.List;
+
 public interface Dao {
+    public List<Asteroid> getAsteroids();
+    public Asteroid getAsteroid(int ID);
+    public List<Asteroid> getAsteroidsByDiameterPlus(double diameter);
+    public List<Asteroid> getAsteroidsByVelocityPlus(double velocity);
+    public List<Asteroid> getHazardusAsteroids();
 }
